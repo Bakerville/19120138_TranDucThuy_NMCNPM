9f7840f52d7fec043130a01678617794f025532d
Bakerville <tdthuycg2001@gmail.com>
2022-04-08 16:16:17 +0700

    Third commit

--- midterm/accounts/models.py
+++ midterm/accounts/models.py
@@ -43,3 +43,6 @@ class Order(models.Model):
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
 	tags = models.ManyToManyField(Tag)
+
+	def __str__(self):
+		return self.product.name
--- midterm/accounts/urls.py
+++ midterm/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.home),
-    path("products/", views.products),
-    path("customers/<str:pk_test>/", views.customer),
+    path("", views.home, name="home"),
+    path("products/", views.products, name="products"),
+    path("create_order/<str:pk>", views.createOrder, name="create_order"),
+    path("update_order/<str:pk>", views.updateOrder, name="update_order"),
+    path("customers/<str:pk_test>/", views.customer, name="customers"),
+    path("delete_order/<str:pk>/", views.deleteOrder, name="delete_order"),
 ]
